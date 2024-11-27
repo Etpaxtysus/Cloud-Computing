074ffa2ed8c20bcbb0505b6a9e66fbd41c24ccf4
Etpaxtysus <itsrhenaldy@gmail.com>
2024-11-28 02:24:47 +0700

    fix: typo and debug

--- Task-1/opentofu/main.tf
+++ Task-1/opentofu/main.tf
@@ -95,4 +95,15 @@ resource "libvirt_domain" "ubuntu" {
     listen_type = "address"
     autoport    = true
   }
+
+
+
 }
+
+  output "db_server_ip" {
+  value = libvirt_domain.ubuntu[0].network_interface[0].addresses[0]
+  }
+
+  output "web_server_ip" {
+    value = libvirt_domain.ubuntu[1].network_interface[0].addresses[0]
+  }
