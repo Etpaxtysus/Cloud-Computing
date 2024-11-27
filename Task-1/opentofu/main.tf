49bd1de9d6276607a3004da722c06019b80c3a7b
Etpaxtysus <itsrhenaldy@gmail.com>
2024-11-27 21:05:38 +0700

    fix: fix opentofu setup and configuration

--- Task-1/opentofu/main.tf
+++ Task-1/opentofu/main.tf
@@ -1,8 +1,7 @@
 terraform {
   required_providers {
     libvirt = {
-      source  = "dmacvicar/libvirt"
-      version = "0.8.1"
+      source = "dmacvicar/libvirt"
     }
   }
 }
@@ -11,49 +10,67 @@ provider "libvirt" {
   uri = "qemu:///system"
 }
 
-resource "libvirt_pool" "default" {
-  name = "default"
-  type = "dir"
+# resource "libvirt_pool" "default" {
+#   name = "default"
+#   type = "dir"
 
-  target {
-    path = "/var/lib/libvirt/images"
+#   target {
+#     path = "/var/lib/libvirt/images"
+#   }
+# }
+
+resource "null_resource" "cache_image" {
+  provisioner "local-exec" {
+    command = "wget -O /tmp/ubuntu-20.04.qcow2 ${var.ubuntu_image}"
   }
 }
 
-resource "libvirt_volume" "ubuntu" {
-  name   = "ubuntu-20.04.qcow2"
-  pool   = libvirt_pool.default.name
-  source = "https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img"
+resource "libvirt_volume" "base" {
+  name   = "base.qcow2"
+  source = "/tmp/ubuntu-20.04.qcow2"
+  pool   = var.libvirt_disk_path
   format = "qcow2"
+  depends_on = [null_resource.cache_image]
 }
 
-resource "libvirt_cloudinit_disk" "web_server_init" {
-  name       = "web-server-init.iso"
-  pool       = libvirt_pool.default.name
-  user_data  = file("${path.module}/cloud-init-web.yaml")
+resource "libvirt_volume" "ubuntu20-qcow2" {
+  count          = length(var.vm)
+  name           = "ubuntu20-${count.index}.qcow2"
+  base_volume_id = libvirt_volume.base.id
+  pool           = var.libvirt_disk_path
+  size           = 5368709120
 }
 
-resource "libvirt_cloudinit_disk" "db_server_init" {
-  name       = "db-server-init.iso"
-  pool       = libvirt_pool.default.name
-  user_data  = file("${path.module}/cloud-init-db.yaml")
+data "template_file" "user_data" {
+  count    = length(var.vm)
+  template = file("${path.module}/setup/cloud-init.yml")
 }
 
-resource "libvirt_domain" "web_server" {
-  name   = "web-server"
-  memory = 1024
+data "template_file" "network_config" {
+  count    = length(var.vm)
+  template = file("${path.module}/setup/network-config.yml")
+}
+
+resource "libvirt_cloudinit_disk" "commoninit" {
+  count          = length(var.vm)
+  name           = "commoninit-${count.index}.iso"
+  user_data      = data.template_file.user_data[count.index].rendered
+  network_config = data.template_file.network_config[count.index].rendered
+  pool           = var.libvirt_disk_path
+}
+
+resource "libvirt_domain" "ubuntu" {
+  count  = length(var.vm)
+  name   = var.vm_hostnames[count.index]
+  memory = "1024"
   vcpu   = 1
 
-  disk {
-    volume_id = libvirt_volume.ubuntu.id
-  }
-
-  disk {
-    volume_id = libvirt_cloudinit_disk.web_server_init.name
-  }
+  cloudinit = libvirt_cloudinit_disk.commoninit[count.index].id
 
   network_interface {
-    network_name = "default"
+    network_name   = "default"
+    wait_for_lease = true
+    hostname       = var.vm_hostnames[count.index]
   }
 
   console {
@@ -62,39 +79,19 @@ resource "libvirt_domain" "web_server" {
     target_type = "serial"
   }
 
-  cloudinit = "${file("cloud-init-web.yaml")}"
-}
-
-resource "libvirt_domain" "db_server" {
-  name   = "db-server"
-  memory = 1024
-  vcpu   = 1
-
-  disk {
-    volume_id = libvirt_volume.ubuntu.id
-  }
-
-  disk {
-    volume_id = libvirt_cloudinit_disk.db_server_init.name
-  }
-
-  network_interface {
-    network_name = "default"
-  }
-
   console {
     type        = "pty"
-    target_port = "0"
-    target_type = "serial"
+    target_type = "virtio"
+    target_port = "1"
   }
 
-  cloudinit = "${file("cloud-init-db.yaml")}"
-}
+  disk {
+    volume_id = libvirt_volume.ubuntu20-qcow2[count.index].id
+  }
 
-output "web_server_ip" {
-  value = libvirt_domain.web_server.network_interface[0].addresses[0]
-}
-
-output "db_server_ip" {
-  value = libvirt_domain.db_server.network_interface[0].addresses[0]
+  graphics {
+    type        = "spice"
+    listen_type = "address"
+    autoport    = true
+  }
 }
